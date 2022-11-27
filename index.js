341d1aed18c6bd27d0b9fc29262da468e1db64de
Adam Strickland <adamstrickland97@gmail.com>
2022-11-27 17:05:47 -0600

    Add rendering engine + remove standalone

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const express = require('express')
 const { v4 } = require('uuid');
 const got = require('got').default;
-const { JWT: {decode: jwtDecode} } = require('jose');
+const { JWT: { decode: jwtDecode } } = require('jose');
 const app = express()
 const port = 3000
 const debug = require('debug')('fhirUser');
 
-const FHIR_ISS = process.env.FHIR_ISS;
-if (!FHIR_ISS) {
+const isProd = process.env.NODE_ENV == 'production';
+const fhirIss = process.env.FHIR_ISS;
+if (!fhirIss) {
   throw new Error('Missing FHIR_ISS environment variable')
 }
+const launchUrl = process.env.BASE_URL + "/launch";
+const clientId = process.env.CLIENT_ID
+app.set('view engine', 'pug');
 
 const { auth, requiresAuth } = require('express-openid-connect');
+const createHttpError = require('http-errors');
 app.use(
   auth({
-    authorizationParams: {
-      response_type: 'code',
-      aud: FHIR_ISS,
-      scope: 'launch openid fhirUser launch/patient'
-    },
     authRequired: false,
     session: {
       cookie: {
-        // Embedding in EHR iframe requires None, counts as cross-site
+        // Embedding in EHR iframe requires SameSite=None, counts as third party cookie
         // TODO: Implement iframe protection using HTTP response headers
         sameSite: "None"
       }
+    },
+    routes: {
+      // Set to false to turn off standalone launch (/login endpoint)
+      login: false
     },
     getLoginState: () => {
       // New UUID to represent this login
@@ -42,41 +46,45 @@ app.use(
         return Promise.reject('Patient missing from context. Does your auth server support launch/patient scope?');
       } else {
         const id_token = jwtDecode(session.id_token);
-        if (!id_token.fhirUser) {
-          return Promise.reject('"fhirUser" claim missing');
-        } else {
-          const tabs = req.oidc.isAuthenticated()
-            && req.oidc.user.sub == id_token.sub
-            // Same user logged in, combine tabs
-            ? {
-              [state.tabId]: { patient: session.patient },
-              ...req.appSession.tabs
-            }
-            // New user logged in, create new tabs
-            : {
-              [state.tabId]: { patient: session.patient }
-            };
-          return Promise.resolve({
-            ...session,
-            tabs
-          });
-        }
+        // TODO: add other keys like "encounter". Consider error handling like above
+        const tabData = { patient: session.patient };
+        const isNewUser =
+          req.oidc.isAuthenticated()
+          && req.oidc.user.sub == id_token.sub;
+        const tabs = isNewUser
+          // Same user logged in, combine tabs
+          ? {
+            [state.tabId]: tabData,
+            // REVIEW please. Grab old appSession to add to new session
+            ...req.appSession.tabs
+          }
+          // New user logged in, create new tabs object
+          : {
+            [state.tabId]: tabData
+          };
+        return Promise.resolve({
+          ...session,
+          tabs
+        });
       }
     }
   })
 );
 
-app.get('/launch', (req, res) => {
+// Auth server initiating an authorize request. Per SMART App Launch
+app.get('/launch', (req, res, next) => {
   const req_iss = req.query.iss;
-  if (req_iss != FHIR_ISS) {
-    res.send(`iss mismatch. Expected ${FHIR_ISS} and received ${req_iss}`)
+  if (!req_iss) {
+    next(createHttpError(400, "'iss' parameter missing. Should be " + fhirIss))
+  } else if (req_iss != fhirIss) {
+    next(createHttpError(400, `iss mismatch. Expected ${fhirIss} and received ${req_iss}`))
   } else if (!req.query.launch) {
-    res.send('Missing launch param')
+    next(createHttpError(400, "Missing 'launch' param"))
   } else {
     res.oidc.login({
       authorizationParams: {
         response_type: 'code',
-        aud: FHIR_ISS,
+        aud: fhirIss,
         scope: 'launch openid fhirUser launch/patient',
         launch: req.query.launch
       }
@@ -84,50 +92,91 @@ app.get('/launch', (req, res) => {
   }
 })
 
-app.get('/tab/:tabId/patient', requiresAuth(), async (req, res) => {
-  const requestedTab = req.params.tabId;
-  const tabData = req.appSession.tabs[requestedTab];
-  if (!tabData) {
-    res.send('Requested tab forbidden') //403
-  } else {
-    // Request patient data!
-    const tokenSet = req.oidc.accessToken;
-    const patient = await got(FHIR_ISS + '/Patient/' + tabData.patient, {
-      headers: {
-        Authorization: tokenSet.token_type + ' ' + tokenSet.access_token
-      }
-    }).json();
-    res.json(patient);
-  }
-})
-
 app.get('/tab/:tabId', requiresAuth(), async (req, res) => {
   const requestedTab = req.params.tabId;
   const tabData = req.appSession.tabs[requestedTab];
   if (!tabData) {
-    res.send('Requested tab forbidden') //403
+    next(createHttpError(403, 'Requested tab forbidden'))
   } else {
-    res.send('Logged in as ' + req.oidc.user.fhirUser + ' with patient ' + tabData.patient
-      + `, try browsing to ${process.env.BASE_URL}/tab/${requestedTab}/patient`) //400
+    // Request patient data!
+    const tokenSet = req.oidc.accessToken;
+    const patient = await got(fhirIss + '/Patient/' + tabData.patient, {
+      headers: {
+        Authorization: tokenSet.token_type + ' ' + tokenSet.access_token
+      }
+    }).json();
+    // TODO, error handling around name parsing
+    const nameObject = patient.name[0]; 
+    const name = nameObject.family + ", " + nameObject.given[0];
+    res.render('tab', {
+      title: "Seeing " + name,
+      message: "Seeing Patient " + name,
+      patient: JSON.stringify(patient, null, 2)
+    })
   }
 })
 
-app.get('/appSession', (req, res) => {
+app.get('/', (req, res, next) => {
   if (!req.oidc.isAuthenticated()) {
-    res.send('Not Logged In')
+    res.render('login', {
+      title: "Log into fhiruser",
+      message: "Launch this app from the EHR to login",
+      fhirIss: fhirIss,
+      launchUrl: launchUrl,
+      clientId: clientId
+    })
   } else {
-    res.json(req.appSession);
+    res.render('index', {
+      title: "Welcome to fhiruser",
+      message: "Logged in with these tabs",
+      tabs: req.appSession.tabs
+    })
   }
 });
 
-app.get('/', (req, res) => {
-  if (!req.oidc.isAuthenticated()) {
-    res.send('Not Logged In')
-  } else {
-    res.send('Logged in as ' + req.oidc.user.fhirUser + ', with these tabs: ' + JSON.stringify(req.appSession.tabs, null, 2));
+if (!isProd) {
+  app.get('/debug', requiresAuth(), (req, res, next) => {
+    res.json({
+      user: req.oidc.user,
+      appSession: req.appSession
+    })
+  });
+  app.get('/debug/:tabId', requiresAuth(), async (req, res, next) => {
+    const requestedTab = req.params.tabId;
+    const tabData = req.appSession.tabs[requestedTab];
+    if (!tabData) {
+      next(createHttpError(403, 'Requested tab forbidden'))
+    } else {
+      // Request patient data!
+      const tokenSet = req.oidc.accessToken;
+      const patient = await got(fhirIss + '/Patient/' + tabData.patient, {
+        headers: {
+          Authorization: tokenSet.token_type + ' ' + tokenSet.access_token
+        }
+      }).json();
+      res.json(patient);
+    }
+  });
+}
+
+// error handlers
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+
+  switch (err.status) {
+    case 404:
+      break;
+    default:
+      debug('Error handled: %o', err);
+      break;
   }
+
+  res.render('error', {
+    message: err.message,
+    error: !isProd ? err : {}
+  });
 });
 
 app.listen(port, () => {
-  debug(`Launch to ${process.env.BASE_URL}/launch. Only iss ${FHIR_ISS} will be accepted`)
+  debug(`Launch to ${launchUrl}. Only iss ${fhirIss} will be accepted`)
 });
